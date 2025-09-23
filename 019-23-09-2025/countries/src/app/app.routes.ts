77b710f8a57f006d5f783dfb0b1f137e125046a5
ramykamalteach <ramy.kamal.teach@gmail.com>
2025-09-23 19:00:37 +0300

    012

--- 019-23-09-2025/countries/src/app/app.routes.ts
+++ 019-23-09-2025/countries/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Countries } from './pages/countries/countries';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'countries/:lang', component: Countries },
+    { path: '**', redirectTo: '' }
+];
