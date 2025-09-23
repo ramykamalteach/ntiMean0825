3130ecab019d984b0b55c8c5b663a049dfcbdb06
ramykamalteach <ramy.kamal.teach@gmail.com>
2025-09-23 18:38:15 +0300

    0011

---
+++ 019-23-09-2025/countries/src/app/services/country.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Country } from '../interfaces/country';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountryService {
+  private apiUrl = 'https://restcountries.com/v3.1/';
+
+  constructor(private http: HttpClient) { }
+
+  getCountries(lang : String): Observable<Country[]> {
+    return this.http.get<Country[]>(`${this.apiUrl}/lang/${lang}`);
+  }
+}
